457ce3661beb316cc2f8a25511d023afea4b1662
Sohailbasha <shlbsh94@gmail.com>
2017-11-16 16:04:38 -0500

    added a model controller + network controller

---
+++ pt/PrayerController.swift
@@ -0,0 +1,14 @@
+//
+//  PrayerController.swift
+//  pt
+//
+//  Created by Ilias Basha on 11/16/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import Foundation
+
+class PrayerController {
+    static let sharedInstance = PrayerController()
+    
+}
