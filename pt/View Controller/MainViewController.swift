cde293104e969d75690f75b5c2898b0e0c81364e
Sohailbasha <shlbsh94@gmail.com>
2017-11-16 13:51:30 -0500

    added programatic collection view cell

--- pt/View Controller/MainViewController.swift
+++ pt/View Controller/MainViewController.swift
@@ -8,24 +8,39 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 1
         layout.minimumLineSpacing = 1
         layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-        let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        collectionView.backgroundColor = UIColor.white
+        let cv = UICollectionView(frame: UIScreen.main.bounds, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.white
+        return cv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(DetailsCollectionViewCell.self, forCellWithReuseIdentifier: "detailCell")
         self.view.addSubview(collectionView)
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "detailCell", for: indexPath) as? DetailsCollectionViewCell
+        return cell ?? UICollectionViewCell()
+    }
     
     
     
 
+    
     /*
     // MARK: - Navigation
 
