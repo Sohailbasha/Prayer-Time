a4f9ba874e9b6efb8cc680354b6efe48ba596715
Sohailbasha <shlbsh94@gmail.com>
2017-11-16 16:41:18 -0500

    added SwiftyJSON and AlamoFire frameworks

--- pt/Prayer.swift
+++ pt/Prayer.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 struct Prayer {
     let name: String
@@ -18,6 +20,22 @@ class Prayers: NSObject {
     var baseURL: String = "http://muslimsalat.com/"
     
     func fetch(location: String) {
-        
+        guard let url = URL(string: "\(baseURL)\(location).json") else { return }
+        let parameters = ["key":Keys.apiKey]
+    
+        NetworkController.performRequest(for: url, httpMethod: .get, urlParameters: parameters, body: nil) { (data, error) in
+            if let error = error {
+                print("Error fetching data: \(error)")
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            guard let jsonData = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String:Any] else {
+                return
+            }
+            
+        }
     }
 }
