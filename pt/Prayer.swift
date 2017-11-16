2d65b8aab97315a6e5034399e16990eb6e3c68fc
Sohailbasha <shlbsh94@gmail.com>
2017-11-16 16:01:28 -0500

    added Prayer.swift file

---
+++ pt/Prayer.swift
@@ -0,0 +1,19 @@
+//
+//  Prayer.swift
+//  pt
+//
+//  Created by Ilias Basha on 11/16/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import Foundation
+
+struct Prayer {
+    let name: String
+    let time: String
+}
+
+class Prayers: NSObject {
+    var prayers: [Prayer] = []
+    
+}
