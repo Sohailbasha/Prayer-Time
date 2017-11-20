3aae4c300643da50e4a10404ac26e90f396f0812
Sohailbasha <shlbsh94@gmail.com>
2017-11-20 14:39:00 -0500

    implementing LocationManager.swift file

--- pt/LocationManager.swift
+++ pt/LocationManager.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    
+    var coreLocationManager: CLLocationManager!
+    var currentLocation: CLLocation?
+    
+    func start() {
+        coreLocationManager = CLLocationManager()
+        coreLocationManager.delegate = self
+        coreLocationManager.requestWhenInUseAuthorization()
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            coreLocationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            currentLocation = CLLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        }
+        coreLocationManager.stopUpdatingLocation()
+    }
+}
