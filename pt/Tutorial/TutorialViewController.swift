507b17ff10549e91302d1abc1afda8b055c3accc
Sohailbasha <shlbsh94@gmail.com>
2017-12-05 15:27:29 -0500

    adding ui elements in interface builder instead of creating them  programatically.

--- pt/Tutorial/TutorialViewController.swift
+++ pt/Tutorial/TutorialViewController.swift
@@ -7,68 +7,23 @@
 //
 
 import UIKit
-import MapKit
+import CoreLocation
 
-class TutorialViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+protocol Locatable {
+    func didLocateSuccessfully(location: String)
+}
 
+
+
+class TutorialViewController: UIViewController, CLLocationManagerDelegate {
+    
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setupViews()
-
-        
-        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-        locationManager.delegate = self
+    
+        findMyLocaiton()
     }
-    
-    
-    func findMyLocaiton() {
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        CLGeocoder().reverseGeocodeLocation(manager.location!) { (placeMarks, error) in
-            if let error = error {
-                print("reverse geocoder fialed with error: \(error.localizedDescription)")
-            }
-            if let placeMarks = placeMarks {
-                if placeMarks > 0 {
-                    let pm = placeMarks[0] as? CLPlacemark
-                    //display location
-                } else {
-                    print("problem with the data recieved")
-                }
-            }
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Error while updating location \(error.localizedDescription)")
-    }
-    
-    
-    func setupViews() {
-        self.view.backgroundColor = #colorLiteral(red: 0, green: 0.6189187169, blue: 0.9825027585, alpha: 1)
-        self.view.addSubview(welcomeLabel)
-        completeButton.frame.origin.y = self.view.frame.height - 50
-        self.view.addSubview(completeButton)
-        mapView.center = self.view.center
-        self.view.addSubview(mapView)
-    }
-    
-    let mapView: MKMapView = {
-        let map = MKMapView()
-        map.layer.cornerRadius = 10
-        map.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - UIScreen.main.bounds.width * 0.10, height: UIScreen.main.bounds.height - UIScreen.main.bounds.height * 0.45)
-        return map
-    }()
     
     let welcomeLabel: UILabel = {
         let label = UILabel()
@@ -77,7 +32,7 @@ class TutorialViewController: UIViewController, CLLocationManagerDelegate, MKMap
         label.textAlignment = .center
         label.numberOfLines = 0
         label.textColor = .white
-        label.text = "To recieve accurate prayer times, \nplease enter your city or state"
+        label.text = "Enter a location for, \naccurate times"
         return label
     }()
     
@@ -92,16 +47,108 @@ class TutorialViewController: UIViewController, CLLocationManagerDelegate, MKMap
     
     let locationTextField: UITextField = {
         let textField = UITextField()
-        textField.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 100, height: 50)
+        textField.borderStyle = .roundedRect
+        textField.alpha = 0.90
+        textField.backgroundColor = .white
+        textField.textColor = .black
+        textField.placeholder = "Enter a city or state"
+        textField.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 50, height: 53.16)
         return textField
     }()
     
+    let findMeButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Find Me", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.alpha = 0.5
+        button.frame = CGRect(x: 0, y: 0, width: 100, height: 46)
+        return button
+    }()
 }
 
-protocol Locatable {
-    func didLocateSuccessfully()
+// MARK: - Methods
+extension TutorialViewController: UITextFieldDelegate{
+    
+    func setupViews() {
+        self.view.backgroundColor = #colorLiteral(red: 0, green: 0.6189187169, blue: 0.9825027585, alpha: 1)
+        self.view.addSubview(welcomeLabel)
+        completeButton.frame.origin.y = self.view.frame.height - 50
+        self.view.addSubview(completeButton)
+        locationTextField.center = self.view.center
+        locationTextField.delegate = self
+        self.view.addSubview(locationTextField)
+        findMeButton.center.x = self.view.center.x
+        findMeButton.center.y = locationTextField.center.y + 100
+        self.view.addSubview(findMeButton)
+    }
+    
+    func findMyLocaiton() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    
+    
 }
 
+// MARK: - TextField Delegate Methods
+extension TutorialViewController {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        return true
+    }
+    
+}
+
+
+// MARK: - Location Manager Delegate Methods
+extension TutorialViewController {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        CLGeocoder().reverseGeocodeLocation(manager.location!) { (placeMarks, error) in
+            if let error = error {
+                print("reverse geocoder fialed with error: \(error.localizedDescription)")
+            }
+            if let placeMarks = placeMarks {
+                if placeMarks.count > 0 {
+                    let pm = placeMarks[0] as CLPlacemark
+                    self.getLocaitonInfo(placeMark: pm)
+                } else {
+                    print("problem with the data recieved")
+                }
+            }
+        }
+    }
+    
+    func getLocaitonInfo(placeMark: CLPlacemark?) {
+        if let placeMark = placeMark {
+            locationManager.stopUpdatingLocation()
+            if let locality = placeMark.locality {
+                print(locality)
+            }
+            
+            if let adminArea = placeMark.administrativeArea {
+                print(adminArea)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error while updating location \(error.localizedDescription)")
+    }
+}
 
 
 
