ed9bfe661428c3f1b1d937dd69d5c5453de39267
Sohailbasha <shlbsh94@gmail.com>
2017-12-05 16:26:03 -0500

    setting up delegate methods and wiring up views.

--- pt/Tutorial/TutorialViewController.swift
+++ pt/Tutorial/TutorialViewController.swift
@@ -17,70 +17,62 @@ protocol Locatable {
 
 class TutorialViewController: UIViewController, CLLocationManagerDelegate {
     
-    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         findMyLocaiton()
+        locationTextField.delegate = self
+        locationTextField.layer.cornerRadius = 25
+        locationTextField.clipsToBounds = true
+        findMeButton.layer.cornerRadius = 25
+        
+        
+        completeButton.frame = CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: 50)
+        
+        self.view.addSubview(completeButton)
+        
     }
     
-    let welcomeLabel: UILabel = {
-        let label = UILabel()
-        label.frame = CGRect(x: 0, y: UIScreen.main.bounds.height * 0.05, width: UIScreen.main.bounds.width, height: 150)
-        label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.textColor = .white
-        label.text = "Enter a location for, \naccurate times"
-        return label
-    }()
+    let locationManager = CLLocationManager()
+
     
     let completeButton: UIButton = {
         let button = UIButton()
-        button.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50)
-        button.backgroundColor = .white
         button.setTitle("Complete", for: .normal)
-        button.setTitleColor(#colorLiteral(red: 0.01515489072, green: 0.6201876402, blue: 0.9830837846, alpha: 1), for: .normal)
+        button.backgroundColor = .white
+        button.setTitleColor(#colorLiteral(red: 0, green: 0.6189187169, blue: 0.9825027585, alpha: 1), for: .normal)
+        button.backgroundColor = .white
         return button
     }()
     
-    let locationTextField: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .roundedRect
-        textField.alpha = 0.90
-        textField.backgroundColor = .white
-        textField.textColor = .black
-        textField.placeholder = "Enter a city or state"
-        textField.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 50, height: 53.16)
-        return textField
-    }()
+    var location: String? {
+        didSet {
+            
+        }
+    }
     
-    let findMeButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .black
-        button.setTitle("Find Me", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.alpha = 0.5
-        button.frame = CGRect(x: 0, y: 0, width: 100, height: 46)
-        return button
-    }()
+    var delegate: Locatable?
+    
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var findMeButton: UIButton!
+    
+    @IBAction func locateMeButtonTapped(_ sender: Any) {
+        findMyLocaiton()
+    }
+    
 }
 
 // MARK: - Methods
-extension TutorialViewController: UITextFieldDelegate{
+extension TutorialViewController {
     
-    func setupViews() {
-        self.view.backgroundColor = #colorLiteral(red: 0, green: 0.6189187169, blue: 0.9825027585, alpha: 1)
-        self.view.addSubview(welcomeLabel)
-        completeButton.frame.origin.y = self.view.frame.height - 50
-        self.view.addSubview(completeButton)
-        locationTextField.center = self.view.center
-        locationTextField.delegate = self
-        self.view.addSubview(locationTextField)
-        findMeButton.center.x = self.view.center.x
-        findMeButton.center.y = locationTextField.center.y + 100
-        self.view.addSubview(findMeButton)
+    func completeButtonTapped() {
+        if let location = locationTextField.text, !location.isEmpty {
+            delegate?.didLocateSuccessfully(location: location)
+        } else {
+            // something else
+        }
     }
     
     func findMyLocaiton() {
@@ -90,12 +82,11 @@ extension TutorialViewController: UITextFieldDelegate{
         locationManager.startUpdatingLocation()
     }
     
-    
-    
+
 }
 
 // MARK: - TextField Delegate Methods
-extension TutorialViewController {
+extension TutorialViewController: UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         textField.resignFirstResponder()
@@ -103,11 +94,6 @@ extension TutorialViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        return true
     }
     
 }
@@ -135,12 +121,8 @@ extension TutorialViewController {
     func getLocaitonInfo(placeMark: CLPlacemark?) {
         if let placeMark = placeMark {
             locationManager.stopUpdatingLocation()
-            if let locality = placeMark.locality {
-                print(locality)
-            }
-            
             if let adminArea = placeMark.administrativeArea {
-                print(adminArea)
+                delegate?.didLocateSuccessfully(location: adminArea)
             }
         }
     }
